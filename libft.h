b6921cf25dcf0743401075b03e14ef45cbc250ed
abbiz <AlexisBizeau@MacBook-Pro-de-Alexis.local>
2015-12-17 12:31:03 +0100

    modif

--- libft.h
+++ libft.h
@@ -20,8 +20,8 @@
 # define BUFF_SIZE 42
 
 # include <unistd.h>
-# include <string.h>
 # include <stdlib.h>
+# include <string.h>
 
 typedef struct	s_list
 {
@@ -35,9 +35,10 @@ void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *src, const void *dst, size_t n);
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
-void	*ft_memalloc(size_t size);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+void	*ft_memalloc(size_t size);
+void  	ft_memdel(void **ap);
 
 size_t	ft_strlen(const char *s);
 char	*ft_strdup(const char *s1);
@@ -63,17 +64,6 @@ int		ft_isspace(int c);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
 
-void	ft_putchar(char c);
-void	ft_putchar_fd(char c, int fd);
-void	ft_putstr(char const *s);
-void	ft_putstr_fd(char const *s, int fd);
-void	ft_putendl(char const *s);
-void	ft_putendl_fd(char const *s, int fd);
-void	ft_putnbr(int n);
-void	ft_putnbr_fd(int n, int fd);
-
-void	*ft_memalloc(size_t size);
-void  	ft_memdel(void **ap);
 char	*ft_strnew(size_t size);
 void	ft_strdel(char **as);
 void	ft_strclr(char *s);
@@ -88,6 +78,15 @@ char  	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s);
 char	**ft_strsplit(char const *s, char c);
 char	*ft_itoa(int n);
+
+void	ft_putchar(char c);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr(char const *s);
+void	ft_putstr_fd(char const *s, int fd);
+void	ft_putendl(char const *s);
+void	ft_putendl_fd(char const *s, int fd);
+void	ft_putnbr(int n);
+void	ft_putnbr_fd(int n, int fd);
 
 t_list	*ft_lstnew(void const *content, size_t content_size);
 void	ft_lstdelone(t_list **alst, void (*del)(void*, size_t));
