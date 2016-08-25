1e494a75817b74d63aed31475132cb2a79a7c814
Alexis BIZEAU <abizeau@e3r1p1.42.fr>
2016-08-25 07:55:30 +0200

    push

--- ft_putchar_unicode.c
+++ ft_putchar_unicode.c
@@ -6,7 +6,7 @@
 /*   By: abizeau <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/25 07:39:16 by abizeau           #+#    #+#             */
-/*   Updated: 2016/08/25 07:39:51 by abizeau          ###   ########.fr       */
+/*   Updated: 2016/08/25 07:55:22 by abizeau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,3 +16,17 @@ void	ft_putchar(char c)
 {
 		write(1, &c, 1);
 }
+
+int		main(void)
+{
+	ft_putchar('é');
+	return (0);
+}
+
+/*
+int		main(int argc, char **argv)
+{
+	if (argc == 2)
+		ft_putchar(argv[1]);
+	return (0);
+}*/
